f38b7c5c7c02416b1d95f22c294ed29b17456322
Woler <hristo_den@abv.bg>
2022-09-22 23:37:45 +0300

    structure of the logfic

---
+++ The Dungeon/Controller/DungeonMaster.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Dungeon.Controller
+{
+    public class DungeonMaster
+    {
+        public string JoinParty(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+        public string AddItemToPool(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+        public string PickUpItem(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+        public string UseItem(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+        public string UseItemOn(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+        public string GiveCharacterItem(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+        public string GetStats()
+        {
+            throw new NotImplementedException();
+        }
+        public string Attack(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+        public string Heal(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+        public string EndTurn(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+        public bool IsGameOver()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
